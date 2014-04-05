05c13f98c3d452e9339a65ad7007a5ab74f92a2f
Thomas Helmke <thomas.helmke@gmx.de>
2014-04-05 23:12:07 +0200

    Changed main view to QTableView. Added date picker.

--- pointsdiary.cpp
+++ pointsdiary.cpp
@@ -4,12 +4,16 @@
 #include <QStandardPaths>
 #include <QFile>
 #include <QString>
+#include <QDate>
 
 PointsDiary::PointsDiary(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::PointsDiary)
 {
     ui->setupUi(this);
+    ui->tableView->setAttribute(Qt::WA_AcceptTouchEvents);
+    QDate today = QDate::currentDate();
+    ui->dateEdit->setDate(today);
     QFile file("/sdcard/PointsDiary/test.txt");
     file.open(QIODevice::ReadWrite);
     ui->lineEdit->setText(QString::number(file.exists()));
