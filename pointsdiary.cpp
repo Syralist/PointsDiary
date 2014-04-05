28a56718afdb2dc4b9a28babf078d68ecd8952f8
Thomas Helmke <thomas.helmke@gmx.de>
2014-04-05 17:01:11 +0200

    First try to open a file.

--- pointsdiary.cpp
+++ pointsdiary.cpp
@@ -1,11 +1,18 @@
 #include "pointsdiary.h"
 #include "ui_pointsdiary.h"
 
+#include <QStandardPaths>
+#include <QFile>
+#include <QString>
+
 PointsDiary::PointsDiary(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::PointsDiary)
 {
     ui->setupUi(this);
+    QFile file("/sdcard/PointsDiary/test.txt");
+    file.open(QIODevice::ReadWrite);
+    ui->lineEdit->setText(QString::number(file.exists()));
 }
 
 PointsDiary::~PointsDiary()
